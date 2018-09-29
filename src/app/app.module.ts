9d1c203d834305100902b723527d01bcabeab363
clioo <jesus_acosta1996@hotmail.com>
2018-09-28 18:46:42 -0600

    serve funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-<<<<<<< HEAD
 //paginas
-=======
-
->>>>>>> c2d1aeb10b0ecb5df9ebf2798e9a98797e8c6a43
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -15,14 +11,11 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-<<<<<<< HEAD
 
 //permisos nativos
 import { LocalNotifications } from '@ionic-native/local-notifications';
 import { Camera } from '@ionic-native/camera';
 
-=======
->>>>>>> c2d1aeb10b0ecb5df9ebf2798e9a98797e8c6a43
 @NgModule({
   declarations: [
     MyApp,
@@ -46,13 +39,9 @@ import { Camera } from '@ionic-native/camera';
   providers: [
     StatusBar,
     SplashScreen,
-<<<<<<< HEAD
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     LocalNotifications,
     Camera
-=======
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
->>>>>>> c2d1aeb10b0ecb5df9ebf2798e9a98797e8c6a43
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera'; 
 declare var google:any;
-=======
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
->>>>>>> c2d1aeb10b0ecb5df9ebf2798e9a98797e8c6a43
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-<<<<<<< HEAD
   @ViewChild('map')mapRef:ElementRef;
   @ViewChild('boton')botonRef:ElementRef;
   image: string = null;
@@ -95,7 +88,6 @@ export class HomePage {
      var centerControlDiv = document.createElement('div');
      console.log(centerControlDiv)
      this.CenterControl(centerControlDiv, this.map);
-     this.map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(this.botonRef.nativeElement );
      this.map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(centerControlDiv );
   }
   constructor(public navCtrl: NavController,private camera: Camera) {
@@ -103,11 +95,5 @@ export class HomePage {
   
 
 
-=======
-
-  constructor(public navCtrl: NavController) {
-
-  }
->>>>>>> c2d1aeb10b0ecb5df9ebf2798e9a98797e8c6a43
 
 }
